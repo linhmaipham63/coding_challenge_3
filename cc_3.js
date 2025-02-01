47054363e498ec8067ade71065611cc6af6d8f1b
Linh Pham <linhmaipham@usf.edu>
2025-02-01 14:16:17 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,13 @@
+// Task 1: Product Price Management Scenario: Online Store 
+
+// Declare array prices with five numerical values
+let prices = [3.65,6.78,5.23,2.1,4]; 
+
+// Add a new price to the array
+prices.push(9.12); 
+
+// Remove the first price from the array
+prices.shift(); 
+
+// Log the updated price list
+console.log("Updated Price List:", prices); 
