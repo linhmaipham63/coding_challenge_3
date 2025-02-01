ddedd9183cbf2c8b0bde87f3d1ea46ba500ee8d1
Linh Pham <linhmaipham@usf.edu>
2025-02-01 14:17:31 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -11,3 +11,19 @@ prices.shift();
 
 // Log the updated price list
 console.log("Updated Price List:", prices); 
+
+
+// Task 2: Modifying Customer Orders Scenario: Order List 
+
+// Declare array orders with five numerical values representing quantities
+let orders = [5,6,7,8,9]; 
+
+// Increase the third order quantity by 5
+orders[2] += 5; 
+
+// Calculate the total number of all orders
+let total_order = orders.reduce((sum,order) => sum+order, 0); 
+
+// Print the updated array and total order count
+console.log("Updated array:", orders); 
+console.log("Total order count:", total_order); 
